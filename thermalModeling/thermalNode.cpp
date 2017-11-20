65805174113a1a39d1e0e4d8860172665fc8abc8
Team900 Jetson TX1 <progammers@team900.org>
2017-11-20 16:14:01 -0500

    Added outline of structure of thermal model

---
+++ thermalModeling/thermalNode.cpp
@@ -0,0 +1,72 @@
+#include <vector>
+#include <iostream>
+#include "thermalNode.h"
+#include "spline-master/src/spline.h"
+#include <cstdio>
+#include <cstdlib>
+
+using namespace std;
+
+
+thermalModel::thermalModel(vector<double> _thermalCapacities, vector<vector<int>> _connectionIDs, vector<vector<double>> _viewFactors, vector<double> _emissivities, double _initialKTemp, array<vector<double>, 2> _efficiencyCurve, vector<double> _percentLossAbsorbtion, vector<double> _outsideExposureCoefficient, vector<double> _fanExposureCoefficient, double _gearboxEfficiency)
+{
+	for(int i = 0; i < _thermalCapacities.size(); i++)
+	{
+		node newNode;
+		newNode.thermalCapacity = _thermalCapacities.at(i);	
+		newNode.emissivity	= _emissivities.at(i)
+		newNode.lossAbsorb 	= _percentLossAbsorbtion.at(i);
+		newNode.exposure        = _outsideExposureCoefficient.at(i);
+		newNode.fanExposure     = _fanExposureCoefficient.at(i);
+		
+		
+		for(int j = 0; j < _connectionIDs.at(i).size(); j++)
+		{
+			connection newConnec;
+			newConnec.ID 		= _connectionIDs.at(i).at(j);
+			newConnec.viewFactor    = _viewFactors.at(i).at(j);
+			newNode.connections.push_back(newConnec);
+		}
+		nodes.push_back(newNode); 	//Add node	
+		
+		temps.push_back(intialKTemp);	//Set node temp
+	}
+	
+	gearboxEfficiency = _gearboxEfficiency;
+	efficiencyCurve.set_points(_effeciencyCurve[0], _effeciencyCurve[1]);
+}
+void iterModel(double dt, double current, double voltage, double rotationRate)
+{
+	//this operation and every suboperation needs to be highly efficient, run time critical
+	
+	vector<double> heatAdd;
+	heatAdd = diffuseTemp();
+	for(int i  = 0; i < nodes.size(); i++)
+	{
+		heatAdd.at(i) += efficiencyCurve(rotationRate)*node.at(i).lossAbsorb*gearboxEfficiency; //Add heat from power losses
+		//TODO: check this/improve
+		//Take into account inertia and store it as heat for bookkeeping? There is probably a better way but still.
+	} 
+	distributeHeat(dissipateHeat);		
+	distributeHeat(heatAdd);
+}
+vector<double> thermalModel::diffuseHeat();
+{
+	//Uses temps and nodes.at(i).connections to diffuse. Returns delta heat.
+	//Run time critical
+
+
+}
+vector<double> thermalModel::dissipateHeat()
+{
+	//Uses temps and nodes.at(i).exposure and nodes.at(i).fanExposure to dissipate heat. Returns delta heat.
+	//Run time critical
+}
+void thermalModel::distributeHeat(vector<double> &heats)
+{
+	for(int i = 0; i < heats.size(); i++)
+	{
+		temps.at(i) += heats.at(i) / nodes.at(i).thermalCapacity	
+	}
+	//Run time critical
+}
---
+++ thermalModeling/thermalNode.h
@@ -0,0 +1,48 @@
+#ifndef INC_THERMAL_MODEL
+#define INC_THERMAL_MODEL
+#include <vector>
+#include <array>
+#include <math.h>
+#include <stdlib.h>
+#include "900Math.h"
+#include <Eigen/Dense>
+#include "spline-master/src/spline.h"
+
+
+
+using namespace std;
+
+//All temp is in Kelvin
+
+
+class thermalModel
+{
+	struct connection //Characteristics of a given heat flow from one node to another (should this include fan stuff?)
+	{
+		int ID;
+		double viewFactor;
+		//TODO, add all needed info here for heat transfer between motors
+		
+	};
+	struct node //Characterisitics of a node of the thermal model
+	{
+		double thermalCapacity;
+		vector<connection> connections;
+		double emissivity;
+		double lossAbsorb;
+		double exposure;
+		double fanExposure;
+	}
+	public:	
+		vector<double> temps;
+		void iterModel(double dt, double current, double voltage, double rotationRate);
+	private:
+		//void adjustTemp(double heat, int ID); //Not needed I think.
+		vector<node> nodes;
+		double gearboxEfficiency;
+		void distributeHeat(vector<double> heats);
+		tk::spline efficiencyCurve;
+		vector<double> diffuseHeat();					
+		vector<double> dissipateHeat();					
+}; 
+#endif
